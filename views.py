42fdc3ab8dea10429399ef5e3b85b713e942530e
Arth Gupta <72153219+guptaarth87@users.noreply.github.com>
2023-10-18 09:21:24 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # replace 'home' with the name of your home page URL pattern
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # replace 'home' with the name of your home page URL pattern
+    return render(request, 'login.html')
